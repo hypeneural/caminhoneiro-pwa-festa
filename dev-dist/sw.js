399a5cc5d13f3260b0bb07c0aa92c6d0a465ac83
Hype Neural <hypeneural@gmail.com>
2025-06-27 17:16:43 -0300

    v2025.06.27

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-85a116cc'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.dmpga73c50g"
+    "revision": "0.5dsdvstvigo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
